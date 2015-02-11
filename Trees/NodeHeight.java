1bbed577ebbb5c96b2713bd48a575813d9d1274c
jazzycrazzy <jagan.space@gmail.com>
2015-02-11 07:05:14 +0530

    Create NodeHeight.java

---
+++ Trees/NodeHeight.java
@@ -0,0 +1,40 @@
+package jagan.trees;
+
+
+public class NodeHeight {
+
+	private int key;
+	private int height;
+	private NodeHeight left;
+	private NodeHeight right;
+	
+	public NodeHeight(int key){
+		this.key = key;
+		height = 0;
+	}
+	
+	public int getKey(){
+		return key;
+	}
+	public void setKey(int key){
+		this.key = key;
+	}
+	public NodeHeight getLeft(){
+		return left;
+	}
+	public void setLeft(NodeHeight node){
+		left = node;
+	}
+	public NodeHeight getRight(){
+		return right;
+	}
+	public void setRight(NodeHeight node){
+		right = node;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public void setHeight(int height){
+		this.height = height;
+	}
+}
